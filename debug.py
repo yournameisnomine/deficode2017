b94c26d1857601b5dcc80f5fcffb69d4b70e5b1c
Nicolas <31901225+Mistergix@users.noreply.github.com>
2018-03-16 15:09:58 +0100

    Déboguez ce fichier !
    Le fichier contient des erreurs, trouvez-es et corrigez-les !

---
+++ debug.py
@@ -0,0 +1,26 @@
+# ERROR 1
+
+year == int.input("Bienvenue, en quelle année êtes-vous né(e) ? '))
+
+if year <= 1900
+    print ('Wouah ça c'est vieux')
+elif year > 1900 && year < 2020:
+    print ("Ca va")
+elif:
+    print ("Dans le turfu")
+    
+
+# ERROR 2
+
+classy Person:
+  def __initalize__(self, first_name, last_name):
+    self.first = first_name
+    self.last = lname
+  def speak(self):
+  print("My name is + " self.fname + " " + self.last)
+
+me = Person("Brandon", "Walsh")
+you = Person("Ethan", "Reed")
+
+me.speak()
+you.self.speak
