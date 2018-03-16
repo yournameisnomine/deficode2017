25266d735bf08707bb87a66a78326bd363a2afda
yournameisnomine <37451198+yournameisnomine@users.noreply.github.com>
2018-03-16 18:32:13 +0100

    Matei
    j'ai débugé le truc

--- debug.py
+++ debug.py
@@ -1,26 +1,26 @@
 # ERROR 1
 
-year == int.input("Bienvenue, en quelle année êtes-vous né(e) ? '))
+year = int(input("Bienvenue, en quelle année êtes-vous né(e) ? "))
 
-if year <= 1900
-    print ('Wouah ça c'est vieux')
-elif year > 1900 && year < 2020:
+if year <= 1900:
+    print ("Wouah ça c'est vieux")
+elif year > 1900 and year < 2020:
     print ("Ca va")
-elif:
+else:
     print ("Dans le turfu")
     
 
 # ERROR 2
 
-classy Person:
-  def __initalize__(self, first_name, last_name):
+class Person:
+  def __init__(self, first_name, last_name):
     self.first = first_name
-    self.last = lname
+    self.last = last_name
   def speak(self):
-  print("My name is + " self.fname + " " + self.last)
+    print("My name is " + self.first + " " + self.last)
 
 me = Person("Brandon", "Walsh")
 you = Person("Ethan", "Reed")
 
 me.speak()
-you.self.speak
+you.speak()
